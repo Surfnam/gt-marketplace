8e4e383de7b3d7eb4be6ccbcfe7ba8a51568e9ba
Fevin Felix <87610636+FevinFelix@users.noreply.github.com>
2025-04-11 21:59:22 -0400

    Made changes for smoother deployment

--- my-app/backend/routes/listing.js
+++ my-app/backend/routes/listing.js
@@ -21,17 +21,6 @@ router.get('/:id', getListingById)
 // get all listings by seller id
 router.get('/seller/:id', getListingsBySeller)
 
-// Get a specific listing based on its category
-// http://localhost:3000/listing/category/categoryName
-//router.get('/category/:category', getListingByCategory)
-
-// Get a specific listing based on its price range (expecting a min and max)
-// http://localhost:3000/listing/?min=100&max=200
-//router.get('/', getListingByPrice);
-
-// get listings based on condition
-//router.get('/condition/:condition', getListingsByCondition);
-
 // delete listing by id
 router.delete('/:id', deleteListing);
 
--- my-app/src/pages/Register.jsx
+++ my-app/src/pages/Register.jsx
@@ -94,7 +94,7 @@ function Register() {
   const sendUserDataToMongoDB = async (user) => {
     try {
       const response = await axios.post(
-        "http://localhost:3001/api/users/register",
+        `${process.env.REACT_APP_BACKEND_URL}/api/users/register`,
         {
           uid: user.uid,
           email: user.email,
