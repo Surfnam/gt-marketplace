e36254c87c13c0182be234ef110d3ed7ea14b182
qwerty111i <adwaithsramesh@gmail.com>
2025-04-15 14:34:26 -0400

    Add chat mobile design

--- my-app/src/Chat/ChatSidebar/ChatSidebar.jsx
+++ my-app/src/Chat/ChatSidebar/ChatSidebar.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './ChatSidebar.css';
-import { IoIosSearch } from "react-icons/io";
+import { IoIosSearch, IoMdMenu, IoMdClose } from "react-icons/io";
 
 const ChatSidebar = ({ joinRoom, otherUsers, curOtherUser, notifications }) => {
     // Filtering user checking
@@ -10,50 +10,69 @@ const ChatSidebar = ({ joinRoom, otherUsers, curOtherUser, notifications }) => {
         (user.lastMessage && user.lastMessage.toLowerCase().includes(searchTerm.toLowerCase()))
     );
 
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleSidebar = () => {
+        setIsOpen(prevState => !prevState);
+    };
+
+    const handleUserClick = (userEmail) => {
+        joinRoom(userEmail);
+        setIsOpen(false);
+    };
+
     return (
-        <div className="chat-sidebar">
-            <h2>Chats</h2>
-            <div className="chat-search">
-                <IoIosSearch className="icon" />
-                <input 
-                    type="text" 
-                    placeholder="Search Messages" 
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
+        <>
+            <div className="sidebar-toggle" onClick={toggleSidebar}>
+                {isOpen ? (<IoMdClose className="hamburger-icon" />) 
+                : (<IoMdMenu className="hamburger-icon" />)
+                }
             </div>
-            <ul>
-                {filteredUsers.map((otherUser) => (
-                    <div className="border-top">
-                    <li key={otherUser.email} 
-                        onClick={() => joinRoom(otherUser.email)}
-                        className={`user ${curOtherUser === otherUser.email ? 'selected' : ''}`} 
-                    >  
-                        <div className="profile-pic">
-                            <img 
-                                src={otherUser.profilePicture} 
-                                alt={`${otherUser.username}'s profile`} 
-                                className="profile-pic-img" 
-                            />
+
+            <div className={`chat-sidebar ${isOpen ? 'open' : 'collapsed'}`}>
+                <h2>Chats</h2>
+                <div className="chat-search">
+                    <IoIosSearch className="icon" />
+                    <input 
+                        type="text" 
+                        placeholder="Search Messages" 
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                </div>
+                <ul>
+                    {filteredUsers.map((otherUser) => (
+                        <div className="border-top" key={otherUser.email}>
+                            <li 
+                                onClick={() => handleUserClick(otherUser.email)}
+                                className={`user ${curOtherUser === otherUser.email ? 'selected' : ''}`} 
+                            >  
+                                <div className="profile-pic">
+                                    <img 
+                                        src={otherUser.profilePicture} 
+                                        alt={`${otherUser.username}'s profile`} 
+                                        className="profile-pic-img" 
+                                    />
+                                </div>
+                                <div className="user-description">
+                                    <span className={`username ${notifications[otherUser.email]?.count > 0 ? 'notif' : ''}`}>
+                                        {otherUser.username.length > 17 ? otherUser.username.substring(0, 17) + "..." : otherUser.username}
+                                    </span>
+                                    <span className={`message ${notifications[otherUser.email]?.count > 0 ? 'notif' : ''}`}>
+                                        {otherUser.lastMessage.length > 25 ? otherUser.lastMessage.substring(0, 25) + "..." : otherUser.lastMessage}
+                                    </span>
+                                </div>
+                                {notifications[otherUser.email]?.count > 0 && 
+                                    <span className="notif-circle">
+                                        {notifications[otherUser.email].count > 10 ? "10+" 
+                                        : notifications[otherUser.email].count}
+                                    </span>
+                                }
+                            </li>
                         </div>
-                        <div className="user-description">
-                            <span className={`username ${notifications[otherUser.email]?.count > 0 ? 'notif' : ''}`}>
-                                {otherUser.username.length > 17 ? otherUser.username.substring(0, 17) + "..." : otherUser.username}
-                            </span>
-                            <span className={`message ${notifications[otherUser.email]?.count > 0 ? 'notif' : ''}`}>
-                                {otherUser.lastMessage.length > 25 ? otherUser.lastMessage.substring(0, 25) + "..." : otherUser.lastMessage}
-                            </span>
-                        </div>
-                        {notifications[otherUser.email]?.count > 0 && 
-                            <span className="notif-circle">
-                                {notifications[otherUser.email].count > 10 ? "10+" 
-                                : notifications[otherUser.email].count}
-                            </span>
-                        }
-                    </li></div>
-                ))}
-            </ul>
-        </div>
+                    ))}
+                </ul>
+            </div>
+        </>
     )
 }
 
